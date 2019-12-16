db6bee5f55ee14b91f44897f9c03dd6228069017
Arne Seemann <arne.seemann@palasthotel.de>
2019-12-16 16:23:18 +0100

    Mirror the get and delete methods of the dpa API

--- src/WireQAPIConnector.php
+++ src/WireQAPIConnector.php
@@ -6,11 +6,14 @@ namespace Drupal\dpa_digital_wires;
 
 class WireQAPIConnector {
 
-  public function __construct() {
-
-    $config = \Drupal::config('dpa_digital_wires.settings');
-    $this->base_url = $config->get('wireq_base_url');
-    $this->publishing_state = $config->get('publishing_state');
+  public function __construct(string $base_url = null) {
+    if ( is_null($base_url) ) {
+      $config = \Drupal::config( 'dpa_digital_wires.settings' );
+      $this->base_url         = $config->get( 'wireq_base_url' );
+      $this->publishing_state = $config->get( 'publishing_state' );
+    } else {
+      $this->base_url = $base_url;
+    }
   }
 
 
@@ -18,13 +21,20 @@ class WireQAPIConnector {
    * Get entries, which makes them invisble for approximately 30s
    */
   public function fetchEntries() {
+    $url = $this->base_url . 'entries.json';
+    $response = \Drupal::httpClient()->get($url);
+    return $response;
+  }
   }
 
   /**
    * Remove entry from the queue
    *
-   * @param int $receipt
+   * @param string $receipt
    */
-  public function deleteEntry(int $receipt) {
+  public function deleteEntry(string $receipt) {
+    // receipt is the json field named '_wireq_receipt'
+    $url = $this->base_url . '/entries/' . $receipt;
+    $response = \Drupal::httpClient()->post($url);
   }
 }
