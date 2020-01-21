edd5152d024c043377e1dcfbe813ecfe7a814ca4
Arne Seemann <arne.seemann@palasthotel.de>
2020-01-21 16:42:44 +0100

    Basic import for images
    this throws an error everytime an image is expired, we need to work around this

---
+++ src/Plugin/migrate/process/MediaGenerate.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\dpa_digital_wires\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+use Drupal\media\Entity\Media;
+use Drupal\file\Entity\File;
+use Drupal\migrate\MigrateException;
+use Drupal\migrate\ProcessPluginBase;
+
+/**
+ * Generates a media entity from a file and returns the media id.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "media_generate"
+ * )
+ *
+ * To generate the entity it is best to this in a subprocess:
+ *
+ * @code
+ *  field_name:
+ *    -
+ *      plugin: sub_process
+ *      source: field_name
+ *      process:
+ *        target_id:
+ *          -
+ *            plugin: migration_lookup
+ *            source: fid
+ *            migration: upgrade_d7_file
+ *          -
+ *            plugin: media_generate
+ *            destination_bundle: media_bundle
+ *            destination_field: field_media_name
+ * @endcode
+ */
+class MediaGenerate extends ProcessPluginBase {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+		if (!isset($this->configuration['destination_field'])) {
+			throw new MigrateException('Destination field must be set.');
+		}
+		if (!isset($this->configuration['destination_bundle'])) {
+			throw new MigrateException('Destination bundle must be set.');
+		}
+
+		$field = $this->configuration['destination_field'];
+		$bundle = $this->configuration['destination_bundle'];
+
+		/* @var /Drupal/file/entity/File $file */
+		$file = File::load($value);
+		if ($file === NULL) {
+			throw new MigrateException('Referenced file does not exist');
+		}
+
+    // Grab our metadata.
+    $headline = $row->getSourceProperty('headline');
+		$caption = $row->getSourceProperty('caption');
+		$credit = $row->getSourceProperty('credit');
+
+		$media = Media::create([
+			'bundle' => $bundle,
+			'uid' => $file->getOwner()->id(),
+			'status' => '1',
+			'name' => $headline,
+			$field => [
+				'target_id' => $file->id(),
+				'alt' => $caption,
+			],
+		]);
+		$media->save();
+
+    // @todo uncomment this on the final migration: file_delete($file->id());
+
+		return $media->id();
+	}
+
+}
