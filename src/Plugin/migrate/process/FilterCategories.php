42c6e37e7637e9f4430742c86110b64cd221be2f
Enno Welbers <enno.welbers@googlemail.com>
2020-02-24 09:36:30 +0100

    added migration support for terms

---
+++ src/Plugin/migrate/process/FilterCategories.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\dpa_digital_wires\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * Provides a 'FilterCategories' migrate process plugin.
+ *
+ * @MigrateProcessPlugin(
+ *  id = "dpa_filter_categories",
+ *   handle_multiples=true
+ * )
+ */
+class FilterCategories extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $return = [];
+    foreach($value as $element) {
+      $type=$this->configuration['filter_type'];
+      if($element['type']==$type) {
+        $return[] = $element['name'];
+      }
+    }
+    return $return;
+  }
+
+  public function multiple() {
+    return true;
+  }
+
+
+}
