b33b129c2c2db9e841c339a23d43747f984f4f3b
Enno Welbers <enno.welbers@googlemail.com>
2020-06-10 16:05:36 +0200

    migrate queries the source way to often, so we have to cache the content after collecting the data #8

--- src/Plugin/migrate_plus/data_parser/Wireq.php
+++ src/Plugin/migrate_plus/data_parser/Wireq.php
@@ -9,19 +9,31 @@ use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\Json;
 * Provides a 'Wireq' data parser plugin.
 *
 * @DataParser(
-*  id = "wireq"
+*  id = "wireq",
 *  title = @Translation("wireq")
 * )
 */
 class Wireq extends Json {
 
+  protected $data;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->data=null;
+  }
+
+
   protected function getSourceData($url) {
-    $collection=[];
-    do {
-      $data= parent::getSourceData($url);
-      $collection=array_push($data,$collection);
-    }while(count($data)>0);
-    return $collection;
+    if($this->data == null) {
+      $this->data=[];
+      do {
+        $data = parent::getSourceData($url);
+        if(count($data)>0) {
+          array_push($this->data,$data);
+        }
+      }while(count($data)>0);
+    }
+    return $this->data;
   }
 
 }
